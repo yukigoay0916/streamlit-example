385ed36f782a1ed80d828c5bc20e635674ff9f9e
yukigoay0916 <72856168+yukigoay0916@users.noreply.github.com>
2022-09-26 11:55:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,29 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from deta import Deta
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Data to be written to Deta Base
+with st.form("form"):
+    name = st.text_input("Your name")
+    age = st.number_input("Your age")
+    submitted = st.form_submit_button("Store in database")
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Connect to Deta Base with your Project Key
+deta = Deta(st.secrets["deta_key"])
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# Create a new database "example-db"
+# If you need a new database, just use another name.
+db = deta.Base("example-db")
 
-    points_per_turn = total_points / num_turns
+# If the user clicked the submit button,
+# write the data from the form to the database.
+# You can store any data you want here. Just modify that dictionary below (the entries between the {}).
+if submitted:
+    db.put({"name": name, "age": age})
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+"---"
+"Here's everything stored in the database:"
+# This reads all items from the database and displays them to your app.
+# db_content is a list of dictionaries. You can do everything you want with it.
+db_content = db.fetch().items
+st.write(db_content)
